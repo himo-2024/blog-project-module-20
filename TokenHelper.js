026b3895ef599aaf09949177d0530f3f14870f28
himo-2024 <alaminhimo241@gmail.com>
2025-06-17 23:38:17 +0600

    Add files via upload

---
+++ TokenHelper.js
@@ -0,0 +1,32 @@
+
+
+import jwt from "jsonwebtoken";
+
+export const EncodeToken =  (email , user_id )=> {
+  const KEY = process.env.JWT_SECRET;
+  const EXPIRE = {expiresIn:process.enb.JWT_SECRET_EXPIRES_IN};
+  const PAYLOAD = {email ,user_id};
+  return jwt.sign(PAYLOAD,KEY,EXPIRE);
+};
+
+
+export const DecodeToken = (token)=> {
+  try {
+    const KEY = process.env.JWT_SECRET;
+    return jwt.verify(token,KEY);
+  } catch (error) {
+      return null ;
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
