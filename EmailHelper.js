c1b6eb1b2f1222b8e48bbab45b6c705a1417f9c0
himo-2024 <alaminhimo241@gmail.com>
2025-06-17 23:37:58 +0600

    Add files via upload

---
+++ EmailHelper.js
@@ -0,0 +1,29 @@
+
+
+
+
+import nodemailer from "nodemailer";
+
+export const EmailSend = async (emailTo ,emailText , emailSubject) =>{
+      const transporter = nodemailer.createTransport({
+        host:process.env.SMTP_HOST,
+        port:process.env.SMTP_PORT,
+        secure:false,   
+        auth:{
+          user: process.env.BREVO_SMTP_USER ,
+          pass:process.env.BREVO_SMTP_KEY ,
+        }
+      });
+      
+
+      const mailOptions = {
+        from:process.env.EMAIL_FROM ,
+        to:emailTo,
+        subject:emailSubject ,
+        text:emailText
+      };
+     
+      return await transporter.sendMail(mailOptions);
+};
+
+
